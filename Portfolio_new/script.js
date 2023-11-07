a62164231f0e552987d64629ffa00e79831f4a82
Marcos <69857352+Marcos2445@users.noreply.github.com>
2023-11-06 22:41:40 -0300

    Add files via upload

---
+++ Portfolio_new/script.js
@@ -0,0 +1,31 @@
+emailjs.init("hNOeBE6ps5_CUaYke");
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const serviceID = "service_portf";
+  const templateID = "template_e1gt3fa";
+
+  let buttonSubmit = document.querySelector("#form-submit");
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      buttonSubmit.innerHTML = "Enviado com Sucesso!";
+      buttonSubmit.style.backgroundColor = "#00FA9A";
+
+      setTimeout(() => {
+        document.querySelector("#user_name").value = "";
+        document.querySelector("#user_email").value = "";
+        document.querySelector("#message").value = "";
+
+        buttonSubmit.innerHTML = "Enviar";
+        buttonSubmit.style.backgroundColor = "#016266";
+      }, 2000);
+    },
+    (err) => {
+      buttonSubmit.innerHTML = "Por favor, tente novamente!";
+      buttonSubmit.style.backgroundColor = "#FF0000";
+      console.log(JSON.stringify(err));
+    }
+  );
+});
